95be9ec543ca7fcb1b30e26cc8967f38039065e3
Yash Gokakkar <yashgokakkar@gmail.com>
2023-12-28 23:13:48 +0530

    Register page created

--- pro2024/src/index.js
+++ pro2024/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Register from '../src/components/Register';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Register />
   </React.StrictMode>
 );
 
